1685e54fbc74159481126bc1ad333ab8b00a017d
lyra29 <wthuthu47@gmail.com>
2025-08-04 20:38:21 +0630

    update resport.tsx

--- src/components/report/report.tsx
+++ src/components/report/report.tsx
@@ -51,7 +51,9 @@ const ReportTable = () => {
           </tr>
         </thead>
         <tbody>
+          
           {data.map((activity) => (
+            
             <tr
               key={activity.id}
               className="odd:bg-white odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 border-b dark:border-gray-700 border-gray-200"
